febe81adcc8a1916b6e9c8f24faf729f45c18bbc
bob <inanygt@hotmail.com>
2024-01-23 10:58:57 +0100

    create reaction timer

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,29 +1,47 @@
 <template>
-   <div v-if="showBlock" class="bg-red-700 w-60 h-60 m-auto mt-5 flex items-center justify-center rounded-2xl">
+   <div
+      v-if="showBlock"
+      @click="stopTimer"
+      class="bg-red-700 w-60 h-60 m-auto mt-5 flex items-center justify-center rounded-2xl">
       <div class="text-white">Click me</div>
    </div>   
 </template>
 
 <script>
 export default {
-   setup () {
-      return {}
-   },
+   // Props
    props: ['delay'],
+   // Data
    data() {
       return {
-         showBlock: false
+         showBlock: false,
+         timer: null,
+         reactionTime: 0
       }
    },
+   // Methods
+   methods: {
+      startTimer() {
+         this.timer = setInterval(() => {
+            this.reactionTime += 10
+         }, 10)
+      },
+      stopTimer() {
+         clearInterval(this.timer)
+         console.log(this.timer)
+         console.log(this.reactionTime)
+      }
+   },
+   // Lifecycle hooks 
    mounted() {
       console.log('Block mounted'),
       setTimeout(() => {
          this.showBlock = true
-         console.log(this.delay)
       }, this.delay);
    },
    updated() {
       console.log('Block updated')
+      this.startTimer();
    },
    unmounted() {
       console.log('Block unmounted')
