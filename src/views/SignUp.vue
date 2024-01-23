436000e6aa99b40dc188c4cd7f2a588de1133059
bob <inanygt@hotmail.com>
2024-01-23 14:30:53 +0100

    checkboxes v-model

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -15,12 +15,38 @@
             <option value="developer">Developer</option>
             <option value="designer">Designer</option>
          </select>
+
+         <br/>
+         <br/>
+
+         <div>
+            <input type="checkbox" required v-model="terms">
+            <label class="px-2">Accept terms and conditions</label>
+         </div>
+
+         <br/>
+         <br/>
+
+         <div >
+            <input type="checkbox" value="remote" v-model="job">
+            <label class="px-2">Remote</label>
+         </div>
+         <div>
+            <input type="checkbox" value="hybrid" v-model="job">
+            <label class="px-2">hybrid</label>
+         </div>
+         <div>
+            <input type="checkbox" value="on-site" v-model="job">
+            <label class="px-2">on site</label>
+         </div>
       </form>
 
       <div class="mt-10">
          <p> Email: {{ email }} </p>
          <p> Password: {{ password }} </p>
          <p> Role: {{ role }} </p>
+         <p> Terms: {{ terms }} </p>
+         <p> job: {{ job }} </p>
       </div>
    </div>
 </template>
@@ -34,12 +60,16 @@
          return {
             email: '',
             password: '',
-            role: 'developer'
+            role: 'developer',
+            terms: false,
+            job: [],
          }
+      },
+      methods: {
       }
    }
 </script>
 
-<style scoped>
+<style >
 
 </style>
