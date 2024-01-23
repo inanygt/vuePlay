992f891bcbc493502583a4f88c0532ff3499bb80
bob <inanygt@hotmail.com>
2024-01-23 14:08:12 +0100

    v-model

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -2,8 +2,16 @@
    <div class="mt-10 m-auto w-11/12">
       <form>
          <label> Email:</label>
-         <input ref="email" type="email">
+         <input ref="email" type="email" v-model="email">
+
+         <label> Password:</label>
+         <input ref="password" type="password" v-model="password">
       </form>
+
+      <div class="mt-10">
+         <p> Email: {{ email }} </p>
+         <p> Password: {{ password }} </p>
+      </div>
    </div>
 </template>
 
@@ -11,6 +19,12 @@
    export default {
       mounted() {
          this.$refs.email.focus();
+      },
+      data() {
+         return {
+            email: '',
+            password: ''
+         }
       }
    }
 </script>
