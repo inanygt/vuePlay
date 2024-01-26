08ca07f98e333e3ef5a8529c65ebf8c607891503
bob <inanygt@hotmail.com>
2024-01-27 00:24:17 +0100

    computed

--- src/views/Composition.vue
+++ src/views/Composition.vue
@@ -3,23 +3,48 @@
       <p>hello my name is {{ name }} and i am {{ age }} years old</p>
       <button class="mt-3 py-1 px-2 bg-green-400 rounded-xl" @click="handleClick">Click me</button>
       <input class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-xl py-2 px-2 mx-3 " type="text" v-model="name">
+
+      <br>
+      <br>
+
+      <!-- Computed properties -->
+      <input class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-xl py-2 px-2 mx-3" type="text" v-model="search">
+      <div>search the name - {{ search }}</div>
+
+      <div v-if="search">
+         <div v-for="name in matchingNames" :key="name">
+         {{ name }}
+         </div>
+      </div>
    </div>
+
+  
 </template>
 
 <script >
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 
 export default {
    setup() {
       const name = ref('inan')
       const age = ref(30)
+      const search = ref('')
+
+      // Computed
+
+      const names = ref(['inan', 'pieter', 'laurens', 'tom', 'nick', 'jaak', 'mars'])
+
+      const matchingNames = computed(() => {
+         return names.value.filter((name) => name.includes(search.value) )
+      })
+
 
       const handleClick = () => {
          name.value = 'Mario'
          age.value++
       }
 
-      return { name, age, handleClick }
+      return { name, names, search, matchingNames, age, handleClick }
    }
 }
 
